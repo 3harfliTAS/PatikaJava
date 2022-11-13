55fea9ecf8955905f2f904054b68b76363e4b44f
Aydın TAŞ <aydin.220@hotmail.com>
2022-11-13 18:58:11 +0300

    Update hipo.java

--- hipo.java
+++ hipo.java
@@ -4,19 +4,17 @@ public class Main {
     public static void main(String[] args) {
         int a,b;
         double c;
+        
         Scanner inp = new Scanner(System.in);
 
         System.out.println("1.kenar: ");
         a = inp.nextInt();
+        
         System.out.println("2.kenar: ");
         b = inp.nextInt();
-
+        
         c = Math.sqrt((a*a) +( b*b));
-
+        
         System.out.println("Hipotenus: "+c);
-
-
-
-
     }
 }
