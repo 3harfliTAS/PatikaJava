5d46230ac236166aff6547b2daa68edd74e012f2
Aydın TAŞ <aydin.220@hotmail.com>
2022-11-23 11:02:24 +0300

    Update boxer.java

--- boxer.java
+++ boxer.java
@@ -1,3 +1,61 @@
+public class Ring {
+    boxer b1;
+    boxer b2;
+    int minWeight;
+    int maxWeight;
+
+    public Ring(boxer b1, boxer b2, int minWeight, int maxWeight) {
+        this.b1 = b1;
+        this.b2 = b2;
+        this.minWeight = minWeight;
+        this.maxWeight = maxWeight;
+    }
+
+    public boolean checkWeight(){
+        return(b1.weight >= minWeight && b1.weight <= maxWeight) && (b2.weight >= minWeight && b2.weight <= maxWeight);
+    }
+
+    public void run(){
+        if(checkWeight()) {
+            while (b1.health > 0 && b2.health > 0) {
+
+                System.out.println("----NEW ROUND");
+                b2.health = b1.hit(b2);
+
+                if (isWin()) {
+                    break;
+                }
+                printScore();
+            }
+        }
+        else {
+            System.out.println("Sporcularin agirliklari uyusmuyor");
+        }
+    }
+
+    public boolean isWin(){
+        if(b1.health ==0){
+            System.out.println("Maci kazanan "+ this.b2.name);
+            return true;
+        }
+        else if(b2.health==0){
+            System.out.println("Maci kazanan "+ this.b2.name);
+            return true;
+        }
+        return false;
+    }
+    public void printScore(){
+        System.out.println("---------");
+        System.out.println(b1.name+ " Kalan Can \t "+ this.b1.health);
+        System.out.println(b2.name+ " Kalan Can \t "+ this.b2.health);
+    }
+
+
+}
+
+
+
+
 public class boxer {
     String name;
     int damage, health,weight;
